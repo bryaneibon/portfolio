03b3dea52722673c1274c24da92685c022311f84
bryaneibon <bryaneibon@hotmail.fr>
2021-05-07 15:14:21 -0400

    create pop-ups with more detailed information about our projects and work experience

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+﻿const open_window = document.getElementById("open-window");
+const modal_container = document.getElementById("window_container");
+const close_window = document.getElementById("close-window");
+
+open_window.addEventListener('click', () => {
+    modal_container.classList.add('show');
+})
+
+close_window.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+})
