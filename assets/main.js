4f4e6ba88060d83c7f5a32e8b65f80e32d33f65c
bryaneibon <bryaneibon@hotmail.fr>
2021-05-13 09:28:33 -0400

    Let's create create a burger menu & animation to make the header more responsive.

--- assets/main.js
+++ assets/main.js
@@ -9,3 +9,33 @@ open_window.addEventListener('click', () => {
 close_window.addEventListener('click', () => {
     modal_container.classList.remove('show');
 })
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav.
+        nav.classList.toggle('nav-active');
+
+        //Animate Links.
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = "navLinkFade 0.5s ease forwards ${index / 7 + 1}s" // will start at 1s instead of 0s.
+                console.log(index / 7 + 1);
+            }
+        })
+
+        //Burger Animation...
+        burger.classList.toggle('rotateBurger');
+    });
+}
+
+const mainApp = () => {
+    navSlide();
+}
+
+mainApp();
